876b6f23b642abf652efe44f588bc10abde6ec5f
Abigael <fambabigael38@gmail.com>
2024-03-21 14:50:20 +0300

    control flow

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-function scuberGreetingForFeet(distance){
-  // Write your code here!
-  if(distance<=400){
-    return`This one is on me!`
-  }else if((distance>400)&&(distance<=2000)){
-    return`That will be twenty bucks.`
-  }else if((distance>2000)&&(distance<2500)){
-    return`I will gladly take your thirty bucks.`
-  }else{
-    return`No can do.`
-  }
+function scuberGreetingForFeet(distance) {
+  if (distance <400) {
+    return 'This one is on me!';
+  } else if  (distance >400 && distance <=2000) {
+    return 'That will be twenty bucks.';
+  } else if (distance > 2000 && distance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+  return `No can do.`;
+}
 }
 
-function ternaryCheckCity(destination){
-  // Write your code here!
-  return destination === `NYC` ? `Ok, sounds good.`:`No go.`
-}
+function ternaryCheckCity(city) {
+    return city === "NYC" ? (city="Ok, sounds good."): "No go."
+ }
 
-function switchOnCharmFromTip(tip){
-  // Write your code here!
-  switch(tip){
-    case `generous`:
-      return`Thank you so much.`;
-    case `not as generous`:
-      return `Thank you.`;
-    default:
-      return`Bye.`;
-  }
+ function switchOnCharmFromTip(tip) {
+    switch(tip) {
+       case 'generous': 
+        return 'Thank you so much.'
+       break;
+       case 'not as generous': 
+       return 'Thank you.'
+       break;
+        default:
+          return 'Bye.';
 
-}
+ }
+ }
